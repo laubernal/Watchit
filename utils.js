0aa961509d6e5d7417c6415c7ac44f8dd2116f27
laubernal <lbernalrodrguez@gmail.com>
2021-06-22 18:31:06 +0200

    error control improvements

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const debounce = (func, delay = 1000) => {
   let timeoutId;
@@ -17,7 +18,7 @@ const fileExists = async (filename) => {
   try {
     await fs.promises.access(filename);
   } catch (err) {
-    throw new Error(`Could not find the file ${filename}`);
+    throw new Error(chalk.red(`Utils.fileExists \nError: Could not find the file ${filename}`));
   }
 };
 
